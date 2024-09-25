d0ef7eb03507a0e864a8c4565bd82793cc890a86
ArisCool2008 <101974164+ArisCool2008@users.noreply.github.com>
2024-09-25 11:04:28 +0600

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+meme_dict = {
+            "КРИНЖ": "Что-то очень странное или стыдное",
+            "ЛОЛ": "Что-то очень смешное"
+}
+word = input("enter weird word")
+if word in meme_dict():
+    print(meme_dict[word])
+else:
+    print("i dont know that word")
