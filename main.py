2f9045bf2d498341726405c7c4d6321304e544c5
ArisCool2008 <101974164+ArisCool2008@users.noreply.github.com>
2024-10-16 16:45:50 +0600

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное"
-}
-word = input("enter weird word")
-if word in meme_dict():
-    print(meme_dict[word])
-else:
-    print("i dont know that word")
+import telebot
+import SlotBot
+from bottoken import BotToken
+
+API_TOKEN = BotToken()
+
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello Im Bot!")
+
+
+
+@bot.message_handler(commands=['Slotroll'])
+def slot(message):
+    bot.reply_to(message, SlotBot.Slotroll())
+
+bot.infinity_polling()
