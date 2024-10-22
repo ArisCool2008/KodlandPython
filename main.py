8fbe99fea1e37e09beb1d63e05b08551918f687b
ArisCool2008 <101974164+ArisCool2008@users.noreply.github.com>
2024-10-22 17:40:24 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+import random
 import telebot
-import SlotBot
-from bottoken import BotToken
+import os
 
-API_TOKEN = BotToken()
+API_TOKEN = ('7746125940:AAGI75eAmJgPKnaDuarKf30sTaSmk54NUJw')
 
 bot = telebot.TeleBot(API_TOKEN)
+
+chance = (random.randint(0,10))
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, "Hello Im Bot!")
 
+@bot.message_handler(commands=['coder_meme'])
+def sendcodermeme(message):
+    if chance < 8:
+        images = random.choice(os.listdir("coder_memes"))
+        with open ('coder_memes/' + images, 'rb') as f:
+            bot.send_photo(message.chat.id, f)
+    if chance > 8:
+        images = random.choice(os.listdir("Coder_meme"))
+        with open ('Coder_meme/' + images, 'rb') as f:
+            bot.send_photo(message.chat.id, f)
 
 
-@bot.message_handler(commands=['Slotroll'])
-def slot(message):
-    bot.reply_to(message, SlotBot.Slotroll())
-
-bot.infinity_polling()
